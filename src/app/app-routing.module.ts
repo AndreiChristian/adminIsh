bc83b0d6ba0351d39d7dc80c3417d56f51641411
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-15 20:53:39 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FacilitiesComponent } from './facilities/facilities.component';
+import { FacilityCategoryComponent } from './facilities/facility-category/facility-category.component';
+import { FacilityGroupComponent } from './facilities/facility-group/facility-group.component';
+import { FacilityItemComponent } from './facilities/facility-item/facility-item.component';
 
 const routes: Routes = [
-  {path:"facilities", component:FacilitiesComponent}
+  {
+    path: 'facilities',
+    component: FacilitiesComponent,
+    children: [
+      { path: '', component: FacilityCategoryComponent },
+      { path: 'group', component: FacilityGroupComponent },
+      { path: 'item', component: FacilityItemComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
